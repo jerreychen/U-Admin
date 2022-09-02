9e80ee586c6ea6ac2b5617843125fce3c4196ae8
jerreychen <chenjilv@gmail.com>
2022-09-02 23:36:39 +0800

    fixed bug: 新增 iconfont 图标

---
+++ src/components/VuFontIcon/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<i :class="`${iconPrefix} ${name}`" :style="{
+		'color': `${color || ''}`,
+		'font-size': `${size}px`
+	}"></i>
+</template>
+
+<script setup>
+	const props = defineProps({
+		iconPrefix: { type: String, default: 'iconfont' },
+		name: { type: String, required: true },
+		color: { type: String },
+		size: { type: Number, default: 12 }
+	})
+</script>
+
+<style>
+</style>
